c3c12f276432f760a3692a57effbefb1be288745
1787 Orange Robotics <1787 Orange Robotics@DESKTOP-KEBDUTV.orange.local>
2017-01-17 19:00:53 -0500

    Created DriveTrain class

--- 2017-Robot/src/org/usfirst/frc/team1787/robot/Constants.java
+++ 2017-Robot/src/org/usfirst/frc/team1787/robot/Constants.java
@@ -1,5 +1,30 @@
 package org.usfirst.frc.team1787.robot;
 
-public class Constants {
-
+public final class Constants {
+	
+	///
+	// Motors
+	///
+	
+	// Drive Train
+	public static final int MOTOR_DRIVE_FRONT_RIGHT = 1;
+	public static final int MOTOR_DRIVE_BACK_RIGHT = 2;
+	public static final int MOTOR_DRIVE_FRONT_LEFT = 3;
+	public static final int MOTOR_DRIVE_BACK_LEFT = 4;
+	
+	// Pickup Arm Spinner
+	public static final int MOTOR_PICKUP_SPINNER = 5;
+	
+	// Feeder to shooter
+	public static final int MOTOR_FEEDER = 6;
+	
+	// Flywheel
+	public static final int MOTOR_FLYWHEEL = 7;
+	
+	// Turret
+	public static final int MOTOR_TURRET_ADJUST = 8;
+	
+	// Winch
+	public static final int MOTOR_WINCH = 9;
+	
 }
